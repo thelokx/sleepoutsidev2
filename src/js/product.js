628f0990821404acd2f5ed5446534c47e8dd2816
Ricardo Salazar <therlokx@gmail.com>
2025-08-21 21:08:05 +0200

    week1

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || [] ; // prendere la info dal localSotre or prendere un vettore vuoto
+  cartItems.push(product) // aggiungere product al vettore 
+  setLocalStorage("so-cart", cartItems); // impostare il vettore nell localStore
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
