7f76beffd1cddf25c1b224bb2b467946255dea52
Ricardo Salazar <therlokx@gmail.com>
2025-08-23 22:15:29 +0200

    update

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,50 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+export default class ProductDetails {
+    constructor(productId, dataSource){
+        this.productId = productId;
+        this.dataSource = dataSource;
+        this.product = {};
+    }
+    async init(){
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails()
+        // aggiungere un pulsante aggiungi al carrello
+        document.getElementById("addToCart").addEventListener("click", this.addToCart.bind(this)); 
+    }
+    addToCart(){
+        const cartItems = getLocalStorage("so-cart") || [] ; // prendere la info dal localSotre or prendere un vettore vuoto
+        cartItems.push(this.product) // aggiungere product al vettore 
+        setLocalStorage("so-cart", cartItems); // impostare il vettore nell localStore
+    }
+    renderProductDetails(){
+      document.querySelector("#main").innerHTML = productDetailsTemplate(this.product)
+    }
+}
+
+function productDetailsTemplate(product){
+        return `
+        <section class="product-detail">
+        <h3>${product.Brand.Name}</h3>
+
+        <h2 class="divider">${product.NameWithoutBrand}</h2>
+
+        <img
+          class="divider"
+          src=${product.Image}
+          alt=${product.NameWithotBrand}
+        />
+
+        <p class="product-card__price">${product.ListPrice}</p>
+
+        <p class="product__color">${product.Colors[0].ColorName}</p>
+
+        <p class="product__description">
+          ${product.DescriptionHtmlSimple}
+        </p>
+
+        <div class="product-detail__add">
+          <button id="addToCart" data-id="${product.ID}">Add to Cart</button>
+        </div>
+      </section>
+        `
+    }
--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,10 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import {getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
+const productId = getParam('product');
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [] ; // prendere la info dal localSotre or prendere un vettore vuoto
-  cartItems.push(product) // aggiungere product al vettore 
-  setLocalStorage("so-cart", cartItems); // impostare il vettore nell localStore
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
